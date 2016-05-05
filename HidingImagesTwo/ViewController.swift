8ffaeb88917b5e3f016e7346e6ececd728c2b166
任黛annabell <admin@adminmatoMacBook-Pro.local>
2016-05-04 21:03:12 -0400

    added files

--- HidingImagesTwo/ViewController.swift
+++ HidingImagesTwo/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var greenBird: UIImageView!
+    @IBOutlet weak var redBird: UIImageView!
+    
+    @IBOutlet weak var hideGreenBirdButton: UIButton!
+    @IBOutlet weak var hideRedBirdButton: UIButton!
+    
+    @IBOutlet weak var redBirdButton: UIButton!
+    @IBOutlet weak var greenBirdButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +28,32 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func pushedHideRedBird(sender: AnyObject) {
+        redBird.hidden = true
+        redBirdButton.hidden = false
+        hideRedBirdButton.hidden = true
+    }
+    
 
+    @IBAction func pushedRedBird(sender: AnyObject) {
+        redBird.hidden = false
+        redBirdButton.hidden = true
+        hideRedBirdButton.hidden = false
+    }
+    
+    @IBAction func pushedHideGreenBird(sender: AnyObject) {
+        greenBird.hidden = true
+        hideGreenBirdButton.hidden = true
+        greenBirdButton.hidden = false
+    }
+    
+    @IBAction func pushedGreenBird(sender: AnyObject) {
+        greenBird.hidden = false
+        greenBirdButton.hidden = true
+        hideGreenBirdButton.hidden = false
+    }
+    
 
 }
 
